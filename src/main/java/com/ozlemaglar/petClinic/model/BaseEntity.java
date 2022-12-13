6460a115c37a63bdfeaa2dd553ce1bd4b2f695cd
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2022-12-13 14:39:45 +0300

    added getter, setter

--- src/main/java/com/ozlemaglar/petClinic/model/BaseEntity.java
+++ src/main/java/com/ozlemaglar/petClinic/model/BaseEntity.java
@@ -1,16 +1,20 @@
 package com.ozlemaglar.petClinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.*;
 
-
+@Getter
+@Setter
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(name = "createdDate", updatable = false)
     private Date createdDate;
